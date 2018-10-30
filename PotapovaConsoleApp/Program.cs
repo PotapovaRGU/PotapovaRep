37cd96eb2a6f969a5e92e0b28e7c6d0a94fe1d8e
POTAPOVARGU <POTAPOVARGU@YANDEX.RU>
2018-10-30 09:41:16 +0200

    Update PotapovaConsoleApp/Program.cs

--- PotapovaConsoleApp/Program.cs
+++ PotapovaConsoleApp/Program.cs
@@ -10,7 +10,32 @@ namespace PotapovaConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("хело ворлд");
+            string Ans;
+            do {
+                Console.WriteLine("выберите операцию + - / *");
+                string str = Console.ReadLine();
+                Console.WriteLine("введите первый аргумент");
+                string arg1 = Console.ReadLine();
+                int a1 = int.Parse(arg1);
+                Console.WriteLine("введите первый аргумент");
+                string arg2 = Console.ReadLine();
+                int a2 = int.Parse(arg2);
+                int n=0;
+                switch (str)
+                {
+                    case "+": n = a1 + a2; break;
+                    case "-": n = a1 - a2; break;
+                    case "*": n = a1 * a2; break;
+                    case "/": n = a1 / a2; break;
+                    default: Console.WriteLine("неизвестная ошибка"); break;
+
+
+                };
+                Console.WriteLine("Результат вычислений  {0}", n);
+                Console.WriteLine("Продолжить вычисления ? Y / N");
+                Ans = Console.ReadLine();
+                
+            } while (Ans == "Y");
             Console.ReadKey();
         }
     }
