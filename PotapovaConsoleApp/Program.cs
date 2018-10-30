7c10f176a1ea687d7501181cd863d0150dde7db2
POTAPOVARGU <POTAPOVARGU@YANDEX.RU>
2018-10-30 10:26:23 +0200

    Добавлена стректура и метод подсчета

--- PotapovaConsoleApp/Program.cs
+++ PotapovaConsoleApp/Program.cs
@@ -1,17 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PotapovaConsoleApp
 {
     class Program
     {
+        public struct Kalaulator
+        {
+            public int Paschet(string str, int a1, int a2)
+            {
+                int n = 0;
+                switch (str)
+                {
+                    case "+": n = a1 + a2; break;
+                    case "-": n = a1 - a2; break;
+                    case "*": n = a1 * a2; break;
+                    case "/": n = a1 / a2; break;
+                    default: Console.WriteLine("неизвестная ошибка"); break;
+                };
+                return n;
+            }
+        }
         static void Main(string[] args)
         {
+            var kalk = new Kalaulator();
             string Ans;
-            do {
+            do
+            {
                 Console.WriteLine("выберите операцию + - / *");
                 string str = Console.ReadLine();
                 Console.WriteLine("введите первый аргумент");
@@ -20,22 +34,12 @@ namespace PotapovaConsoleApp
                 Console.WriteLine("введите первый аргумент");
                 string arg2 = Console.ReadLine();
                 int a2 = int.Parse(arg2);
-                int n=0;
-                switch (str)
-                {
-                    case "+": n = a1 + a2; break;
-                    case "-": n = a1 - a2; break;
-                    case "*": n = a1 * a2; break;
-                    case "/": n = a1 / a2; break;
-                    default: Console.WriteLine("неизвестная ошибка"); break;
-
-
-                };
+                int n = kalk.Paschet(str, a1, a2);
                 Console.WriteLine("Результат вычислений  {0}", n);
                 Console.WriteLine("Продолжить вычисления ? Y / N");
                 Ans = Console.ReadLine();
-                
             } while (Ans == "Y");
+
             Console.ReadKey();
         }
     }
